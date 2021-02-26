b76bf131f32e1534cadc1cdcc1080a1e649d398a
jianbao <liujianbao@pudutech.com>
2021-02-26 16:31:27 +0800

    feat(add gd_hal):add gd_hal.c/h
    *增加gd_hal.c/h，用于连接gd标准库和st的hal库
    
    Signed-off-by: jianbao <liujianbao@pudutech.com>

---
+++ HardWare/Inc/gd_hal.h
@@ -0,0 +1,34 @@
+#ifndef GD_HAL_H
+#define GD_HAL_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+#include "gd32f4xx.h"
+
+typedef enum 
+{
+  HAL_OK       = 0x00U,
+  HAL_ERROR    = 0x01U,
+  HAL_BUSY     = 0x02U,
+  HAL_TIMEOUT  = 0x03U
+} HAL_StatusTypeDef;
+
+typedef enum
+{
+  GPIO_PIN_RESET = 0,
+  GPIO_PIN_SET
+}GPIO_PinState;
+
+
+void HAL_GPIO_WritePin(uint32_t gpio_periph,uint32_t pin,GPIO_PinState PinState);
+
+
+GPIO_PinState HAL_GPIO_ReadPin(uint32_t gpio_periph,uint32_t pin);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
