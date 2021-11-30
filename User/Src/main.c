73b66834395515946615b86d8ce0321dee0b7a2a
jianbao <liujianbao@pudutech.com>
2021-11-30 16:55:46 +0800

    feat(version):上次提交增加一个版本
    Signed-off-by: jianbao <liujianbao@pudutech.com>

--- User/Src/main.c
+++ User/Src/main.c
@@ -43,7 +43,7 @@ UINT32 MaxLoopTime = 0;
 void HardwareInit(void);
 BootLoaderInfo bootloaderInfo={0};
 
-ST_VersionStruct NowSoftWareVersion = {21, 0, 18};
+ST_VersionStruct NowSoftWareVersion = {21, 0, 19};
 
 void CAN_MesIAPResetTreatment(BootLoaderInfo* pstbootloaderInfo);
 
