01be88237be7f32a67bce0df0c02d8b8466ecc56
jianbao <liujianbao@pudutech.com>
2022-03-07 13:59:32 +0800

    feat(.bat):修改bat脚本，删除绝对路径
    Signed-off-by: jianbao <liujianbao@pudutech.com>

--- MDK-ARM/Post Universe Chassis.bat
+++ MDK-ARM/Post Universe Chassis.bat
@@ -1,3 +1,3 @@
 @echo off
-C:\Keil_v5\ARM\ARMCC\bin\fromelf.exe --bin --output "bin/Chassis0D_01-xx_xx_xx.bin" "Chasis_Project/Chasis_Project.axf"
+fromelf --bin --output "bin/Chassis0D_01-xx_xx_xx.bin" "Chasis_Project/Chasis_Project.axf"
 CalculateFirmwareCRC 16 3976 "bin\Chassis0D_01-xx_xx_xx.bin"
